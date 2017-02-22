3565f41a6c872e3f574d4fd27bc0a919c1b9fdfe
Shamod Lacoul <shamod@Users-MacBook-Pro.local>
2017-02-21 22:26:37 -0800

    Working Facebook login on iOS and Android

--- new_native/index.ios.js
+++ new_native/index.ios.js
@@ -11,6 +11,7 @@ import {
   Text,
   View
 } from 'react-native';
+import { LoginButton } from 'react-native-fbsdk';
 
 export default class new_native extends Component {
   render() {
@@ -26,6 +27,22 @@ export default class new_native extends Component {
           Press Cmd+R to reload,{'\n'}
           Cmd+D or shake for dev menu
         </Text>
+        <View>
+          <LoginButton
+                publishPermissions={["publish_actions"]}
+                onLoginFinished={
+                  (error, result) => {
+                    if (error) {
+                      alert("Login failed with error: " + result.error);
+                    } else if (result.isCancelled) {
+                      alert("Login was cancelled");
+                    } else {
+                      alert("Login was successful with permissions: " + result.grantedPermissions)
+                    }
+                  }
+                }
+                onLogoutFinished={() => alert("User logged out")}/>
+        </View>
       </View>
     );
   }
